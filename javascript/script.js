9e20fd19ebd511b94de18891ff384efe0b8ddc31
Vampire <vivekvikki10020@gmail.com>
2026-02-27 16:20:57 +0530

    Javascript

---
+++ javascript/script.js
@@ -0,0 +1,58 @@
+// console.log("HOPE");
+// //reinitialize and redeclare a variable
+// var a;
+// a=10;
+// var a;
+// a=20;
+// //Reinitialize
+// let b=10;
+// b=20;
+// //Cant redeclare or reinitialize a constant variable
+// const c=30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="vampire";
+// var bool=true;
+// var un;
+// var nu=null;
+// var big=12345678901234567890n;
+// var symbol=Symbol('li')
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol); 
+
+// var arr=[1,2,3,4,5];
+// console.log(typeof arr);
+
+// var obj={
+//     name:"vampire",
+//     dept:"space"
+// }
+// console.log(obj);
+// console.log(typeof obj);
+    
+var a=10;
+var b='vampire';
+console.log(a+b); //concatenation
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
+
+//relational operators
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
